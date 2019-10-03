d65943ba95598d010579352f7be975191e2bb23d
mroger <rene.mroger@gmail.com>
2019-10-02 19:03:03 -0700

    refactiring, removed scriptinjection

--- temp-extension/background.js
+++ temp-extension/background.js
@@ -1,36 +1,35 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   switch (request.directive) {
     case "save-click":
-      //Read all Tabs currently opened
+      console.log("save");
       getAllTabs().then(tabs => {
-        //work around to pass tabs object to the saver.js script
-        chrome.tabs.executeScript(
-          null,
-          {
-            code: "var tabs = " + JSON.stringify(tabs)
-          },
-          () => {
-            chrome.tabs.executeScript(null, {
-              file: "saver.js"
-            });
-          }
-        );
+        saveData(tabs);
       });
       break;
     case "open-click":
-      chrome.tabs.executeScript(null, {
-        file: "opener.js"
-      });
+      chrome.tabs.create({ url: "http://www.google.com" });
       break;
     case "empty-click":
-      chrome.tabs.executeScript(null, {
-        file: "empty.js"
+      chrome.storage.sync.clear(function() {
+        var error = chrome.runtime.lastError;
+        if (error) {
+          console.error(error);
+        }
+        console.log("all tabs removed");
       });
+
       break;
     case "view-click":
-      chrome.tabs.executeScript(null, {
-        file: "viewer.js"
+      chrome.storage.sync.get(["data"], result => {
+        if (result.data) {
+          for (const tab of result.data[0]) {
+            console.log(tab.url);
+          }
+        } else {
+          console.log("na saved tabs");
+        }
       });
+
       break;
     default:
       alert("Case not found");
@@ -49,5 +48,32 @@ function getAllTabs() {
         resolve(tabs);
       }
     );
+  });
+}
+
+async function saveData(tabs) {
+  console.log("saving");
+  const getStorageData = key =>
+    new Promise((resolve, reject) =>
+      chrome.storage.sync.get(key, result =>
+        chrome.runtime.lastError
+          ? reject(Error(chrome.runtime.lastError.message))
+          : resolve(result)
+      )
+    );
+
+  const { data } = await getStorageData("data");
+
+  const setStorageData = data =>
+    new Promise((resolve, reject) =>
+      chrome.storage.sync.set(data, () =>
+        chrome.runtime.lastError
+          ? reject(Error(chrome.runtime.lastError.message))
+          : resolve()
+      )
+    );
+
+  await setStorageData({ data: [tabs] }).then(() => {
+    console.log("items saved");
   });
 }
--- temp-extension/viewer.js
+++
@@ -1,9 +0,0 @@
-chrome.storage.sync.get(["data"], result => {
-  if (result.data) {
-    for (const tab of result.data[0]) {
-      console.log(tab.url);
-    }
-  } else {
-    console.log("na saved tabs");
-  }
-});
