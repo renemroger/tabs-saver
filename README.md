1a0633618a6a6cdf6306f35ca866b1ec5960b83c
mroger <rene.mroger@gmail.com>
2019-10-04 02:38:59 -0700

    new boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# urlnest
+# Install guide
+
+## Quick Start
+
+```bash
+# Install dependencies for root folder
+npm install
+
+# Install dependencies for client, extension and server
+npm run install-all
+
+# Run the client & server with concurrently
+npm run dev
+
+# Run the Express server only
+npm run server
+
+# Run the React client only
+npm run client
+
+# Run the React client only
+npm run extension
+
+# Server runs on http://localhost:5000 and client on http://localhost:3000
+```
---
+++ server/server.js
@@ -0,0 +1,52 @@
+const Express = require("express");
+const App = Express();
+const BodyParser = require("body-parser");
+const PORT = 8080;
+const { Pool } = require("pg");
+const cors = require("cors");
+require("dotenv").config();
+const axios = require("axios");
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+App.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+pool.connect((error, client) => {
+  console.log(process.env.DB_HOST);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("connected");
+  }
+});
+
+// Express Configuration
+App.use(BodyParser.urlencoded({ extended: false }));
+App.use(Express.static("public"));
+
+// Sample GET route
+App.get("/api/data", (req, res) => {
+  res.json({
+    message: "Seems to work!"
+  });
+});
+
+App.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(
+    `Express seems to be listening on port ${PORT} so that's pretty good 👍`
+  );
+});
